66387b1e5ea852992fc93484159cdebcdd2c55e9
bakar-git <bakar.git@gmail.com>
2026-03-12 01:18:24 +0500

    feat: update console commands for key processing and scheduling

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,22 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\ProcessLiveKeyPids;
+use App\Console\Commands\ProcessStoredKeyPids;
+use App\Console\Commands\ReprocessErrorStoredKeys;
+use App\Console\Commands\ReprocessStoredKeys;
+use App\Console\Commands\UpdateMaxKeysAllowed;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+
+
+Schedule::command(UpdateMaxKeysAllowed::class)->everyFourHours();
+
+Schedule::command(ReprocessStoredKeys::class)->daily();
+Schedule::command(ReprocessErrorStoredKeys::class)->dailyAt('03:00');
+
+
+Schedule::command(ProcessLiveKeyPids::class, ['--sleep=0'])->everyThirtySeconds()->runInBackground()->withoutOverlapping();
+Schedule::command(ProcessLiveKeyPids::class, ['--sleep=5'])->everyThirtySeconds()->runInBackground()->withoutOverlapping();
+
+Schedule::command(ProcessStoredKeyPids::class, ['--sleep=0'])->everyThirtySeconds()->runInBackground()->withoutOverlapping();
+Schedule::command(ProcessStoredKeyPids::class, ['--sleep=5'])->everyThirtySeconds()->runInBackground()->withoutOverlapping();
